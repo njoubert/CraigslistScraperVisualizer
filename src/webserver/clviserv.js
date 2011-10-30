fbade345cb0224adad9ec4d9d1b3ff992f070343
Niels Joubert <njoubert@gmail.com>
2011-10-29 20:08:47 -0700

    configuration now loaded into node

--- src/webserver/clviserv.js
+++ src/webserver/clviserv.js
@@ -3,7 +3,12 @@
 //Created by Niels Joubert on 2011-10-17.
 //Copyright (c) 2011 Stanford. All rights reserved.
 
+var fs = require('fs');
 var connect = require('connect');
+
+var configfile = "../../config.json"
+var configfiledata = fs.readFileSync(configfile)
+var config = JSON.parse(configfiledata)
 
 function handle_data(req,res) {
   console.log("handling data")
@@ -11,5 +16,5 @@ function handle_data(req,res) {
 
 var server = connect.createServer(connect.logger('tiny'));
 server.use('/data', handle_data);
-server.use(connect.static(__dirname + '/../webclient'));
+server.use(connect.static(__dirname + "/../.." + config.STATIC_BASEDIR));
 server.listen(8080);
