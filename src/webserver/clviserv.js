a316c200a98ce334bd2ec87880968d59c4de3e91
Niels Joubert <njoubert@gmail.com>
2011-10-29 19:35:42 -0700

    connect js server is up

--- src/webserver/clviserv.js
+++ src/webserver/clviserv.js
@@ -3,31 +3,10 @@
 //Created by Niels Joubert on 2011-10-17.
 //Copyright (c) 2011 Stanford. All rights reserved.
 
-var base
+var connect = require('connect');
 
-var http = require('http');
-
-function hande_data_req(req, res) {
-  res.writeHead(200);
-  res.end('Hello there data requested');
-  
-}
-
-var server = http.createServer(function(req, res) {
-  
-  console.log(req.url)
-  
-  if (req.url.indexOf("/data") == 0) {
-    handle_data_req(rew, res);
-  } else if () {
-     
-  } else {
-    res.sendHeader(404, {"Content-Type": "text/plain"});  
-    res.write("404 Not Found\n");  
-    res.close();  
-    return;  
-  }  
-  
-});
+var server = connect.createServer(
+  connect.favicon(),
+  connect.logger(),
+  connect.static(__dirname + '/../webclient'));
 server.listen(8080);
-console.log("Server listening on port 8080")
